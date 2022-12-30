d50c0854e9c1b553c4cac55adca6c7b79a1a1df9
mqayum <181400008@gift.edu.pk>
2022-12-30 17:53:33 +0500

    Image url updated to vercel

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -5,7 +5,7 @@ const CategoryCard = ({category}:{category:{categoryName:string, categoryImage:s
         <div className="card sm:w-32 lg:w-44 bg-ghost">
             <Link href="#">
                 <figure className="px-5 hover:-translate-y-1.5 hover:drop-shadow-2xl h-44 transition">
-                    <Image src={category.categoryImage} width={300} height={300} alt="Category" className="rounded-xl" />
+                    <Image src={`https://dim-next-client-6cw21mr48-mqayum.vercel.app${category.categoryImage}`} width={300} height={300} alt="Category" className="rounded-xl" />
                 </figure>
             </Link>
             <div className="flex justify-center items-center text-center mt-3">
